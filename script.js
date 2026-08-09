09dea030a8eea4d5c557b13ce196388028336174
GMinh3004 <skodat299@outlook.com>
2026-08-09 18:30:58 +0700

    Rewrite the code to make it look beautiful

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+let ylog = true;
 
 function getTime() {
     const now = new Date();
@@ -12,6 +13,7 @@ function getTime() {
 
 let foutput = document.getElementById("foutput");
 function jsOut(smessage) {
+    ylog = true;
     const time = getTime();
     foutput.value += `${getTime()} ${smessage}\n`;
     foutput.scrollTop = foutput.scrollHeight;
@@ -22,19 +24,22 @@ jsOut(sram);
 
 let cls = document.getElementById("clear");
 const cpa = document.getElementById("cpA");
+
 cls.addEventListener("click", function () {
     foutput.value = "";
+    ylog = false;
 });
 cpa.addEventListener("click", async function () {
-    if (foutput.value != "") {
+    if (ylog == true) {
         await navigator.clipboard.writeText(foutput.value);
         cpa.innerText = "Copied!";
-        cpa.style.border = "none";
-        cpa.style.backgroundColor = "rgb(0, 230, 0)";
-        cpa.style.color = "white";
-        await wait(3000);
-        cpa.innerText = "Copy All";
+        cpa.classList.add("press");
+
+        setTimeout(() => {
+            cpa.innerText = "Copy All";
+            cpa.classList.remove("press");
+        }, 2000);
     } else {
-        alert("No text :(");
+        alert("No log found!?");
     }
 });
